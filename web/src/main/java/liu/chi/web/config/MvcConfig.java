18350b3a44ba1b78244b31090cf915ba68c1be9e
liuc <648849272@qq.com>
2019-01-03 14:00:33 +0800

    添加序列化配置

--- web/src/main/java/liu/chi/web/config/MvcConfig.java
+++ web/src/main/java/liu/chi/web/config/MvcConfig.java
@@ -1,7 +1,13 @@
 package liu.chi.web.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,8 +15,15 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 /**
+ * web的自动化配置有@EnableWebMvc 与 WebMvcAutoConfigure实现
+ *
  * @author liuchi
+ * @EnableWebMvc===>继承WebMvcConfigurationSupport(二者需要同时存在)
+ * @EnableAutoConfiguration===>实现WebMvcConfigurer接口
  * @date 2018-09-21 13:10
  */
 @Configuration
@@ -61,5 +74,23 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
+    }
+
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        //定制某类型的序列化方式
+        SimpleModule bigDecimalModule = new SimpleModule();
+        bigDecimalModule.addSerializer(BigDecimal.class, ToStringSerializer.instance);
+        objectMapper.registerModule(bigDecimalModule);
+
+        //配置序列化 反序列化
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        jackson2HttpMessageConverter.setObjectMapper(objectMapper);
+        converters.add(jackson2HttpMessageConverter);
     }
 }
